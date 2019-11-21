22e74120bbc1cd785091a53199620fdb21356310
jason-hu7 <dcjasonhu@gmail.com>
2019-11-21 00:47:47 -0600

    collect type hints at the same fiel

---
+++ src/adtk/_typing.py
@@ -0,0 +1,18 @@
+from typing import (
+    Any,
+    Dict,
+    Iterable,
+    Sequence,
+    List,
+    Optional,
+    TypeVar,
+    Union,
+    Mapping
+)
+
+import pandas as pd
+
+# customized types
+TimeSeries = Union[pd.Series, pd.DataFrame]
+
+
